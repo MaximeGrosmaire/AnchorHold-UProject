007cd5563ce5973b7baaa956832e6a8bfa842780
MaximeGrosmaire <maxime.grosmaire@protonmail.com>
2023-02-07 18:56:03 +1100

    Initial commit

---
+++ Source/AnchorHold/AnchorHoldGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AnchorHoldGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANCHORHOLD_API AAnchorHoldGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
